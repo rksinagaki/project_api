03cf0692f8f1004a2c2bb9072c897ae0726dd3ff
takuma <takuma.inagaki@rakus-partners.co.jp>
2025-10-07 17:55:29 +0900

    Add:monitoring code

--- src/app_lambda.py
+++ src/app_lambda.py
@@ -1,11 +1,16 @@
+import logging
+import json
+import sys
 import os
 from io import StringIO
 import boto3
 import pandas as pd
 from googleapiclient.discovery import build
+from aws_lambda_powertools import Logger
 
 # lambdaのeventとは何なのか
 # executation_idとは何なのか
+# 162行目あたりの謎の処理。loggerの環境変数は呼び出さなくてもいいのか。PayloadのoriginalInput.$は結局何をしてんの。
 
 # /////////////////
 # 環境変数読み込み
@@ -19,6 +24,8 @@ youtube = build('youtube',
                 'v3',
                 developerKey=API_KEY
                 )
+logger = Logger()
+
 
 # /////////////////
 # チャンネル情報の取得
@@ -151,33 +158,48 @@ def get_comments_for_video(video_id, max_comments_per_video=100):
 # lambda関数実行
 # /////////////////
 def lambda_handler(event, context):
+    
+    correlation_id_value = event.get('correlation_id')
+
+    if correlation_id_value:
+        logger.set_correlation_id(correlation_id_value)
+
+    current_execution_id = logger.get_correlation_id() 
+    if not current_execution_id:
+        current_execution_id = context.aws_request_id
+
+    logger.info("Lambdaハンドラー処理を開始します。")
+
     s3 = boto3.client('s3',
                       region_name=REGION_NAME)
-    
-    execution_id = event.get('ExecutionId', context.aws_request_id)
 
+    # チャンネルデータの格納
     output_channel = StringIO()
     df_channel = pd.DataFrame(get_channel())
     df_channel.to_json(output_channel, orient='records', lines=True, force_ascii=False)
-    channel_key = f'data/raw_data/{execution_id}/sukima_channel.json'
+    channel_key = f'data/raw_data/{current_execution_id}/sukima_channel.json'
     s3.put_object(
         Bucket=BUCKET_NAME,
         Key=channel_key,
         Body=output_channel.getvalue()
     )
-    print("チャンネル情報をchannel.csvに保存しました。")
+    logger.info("S3へチャンネルデータを保存しました。", 
+                extra={"bucket": BUCKET_NAME, "s3_key": channel_key})
 
+    # ビデオデータの格納
     output_video = StringIO()
     df_videos = pd.DataFrame(get_video())
     df_videos.to_json(output_video, orient='records', lines=True, force_ascii=False)
-    video_key = f'data/raw_data/{execution_id}/sukima_video.json'
+    video_key = f'data/raw_data/{current_execution_id}/sukima_video.json'
     s3.put_object(
         Bucket=BUCKET_NAME,
         Key=video_key,
         Body=output_video.getvalue()
     )
-    print("動画情報をyoutube_videos.csvに保存しました。")
+    logger.info("S3へビデオデータを保存しました。", 
+                extra={"bucket": BUCKET_NAME, "s3_key": video_key})
 
+    # コメントデータの格納
     top_videos_df = df_videos.sort_values(by='view_count', ascending=False).head(100) #本来は100
 
     all_comments = []
@@ -190,13 +212,14 @@ def lambda_handler(event, context):
     output_comment = StringIO()
     df_comments = pd.DataFrame(all_comments)
     df_comments.to_json(output_comment, orient='records', lines=True, force_ascii=False)
-    comment_key = f'data/raw_data/{execution_id}/sukima_comment.json'
+    comment_key = f'data/raw_data/{current_execution_id}/sukima_comment.json'
     s3.put_object(
         Bucket=BUCKET_NAME,
         Key=comment_key,
         Body=output_comment.getvalue()
     )
-    print(f"合計 {len(all_comments)} 件のコメントを保存しました。")
+    logger.info("S3へコメントデータを保存しました。", 
+                extra={"bucket": BUCKET_NAME, "s3_key": comment_key})
 
     return {
             "statusCode": 200,
@@ -206,5 +229,5 @@ def lambda_handler(event, context):
                 f"s3://{BUCKET_NAME}/{video_key}",
                 f"s3://{BUCKET_NAME}/{comment_key}"
             ],
-            "execution_id": execution_id 
+            "correlation_id": current_execution_id 
         }
